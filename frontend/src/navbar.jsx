992037095d7e0d53382a00360fa907e952cfa064
Tawina <tawinankate@gmail.com>
2025-05-26 15:44:06 +0200

    init

---
+++ frontend/src/navbar.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Home,Briefcase,FilePlus,} from 'lucide-react'
+import logoo from './assets/logoo.png' 
+
+
+const Navbar = () => {
+  return (
+    // <div className="w-64 min-h-screen border-r border-gray-500 bg-white p-4">
+    <div className="fixed top-0 left-0 h-screen w-64 border-r border-gray-500 bg-white p-4 z-50">
+        <div className="flex justify-center">
+            <img src={logoo} alt="Logoo" className="h-12 w-auto" />
+        </div>
+
+        <nav className="flex flex-col space-y-6 text-gray-800 text-md">
+            <div className="flex items-center gap-3 hover:text-purple-600 cursor-pointer">
+                <Home size={20} />
+                <span>Home</span>
+            </div>
+
+            <div className="flex items-center gap-3 hover:text-purple-600 cursor pointer">
+                <Briefcase size={20}/>
+                <span>Service</span>
+            </div>
+
+             <div className="flex items-center gap-3 hover:text-purple-600 cursor-pointer">
+                <FilePlus size={20} />
+                <span>Register</span>
+            </div>
+
+            <div className="flex items-center gap-3 hover:text-purple-600 cursor-pointer">
+                <FilePlus size={20} />  
+                <span>More...</span>
+            </div>
+        </nav>
+
+    </div>
+  )
+}
+
+export default Navbar
